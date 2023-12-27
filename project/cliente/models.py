20177adcdc317e97c8c85a0279e46c017fda57c2
motatan79 <m.pirela@gmail.com>
2023-12-27 11:38:28 -0300

    Add gitgnore

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -17,6 +17,8 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length = 100)
     apellido = models.CharField(max_length = 100)
     nacimiento = models.DateField(null= True, blank = True)
+    emailid = models.CharField(max_length=100)
+    telefono = models.CharField(blank=True, null=True, max_length=20)
     pais_origen_id = models.ForeignKey(Pais, null = True, blank = True, on_delete = models.SET_NULL, verbose_name = 'País de origen')
     
     def __str__(self) -> str:
